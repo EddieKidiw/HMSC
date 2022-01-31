cff100ca402b1de2767306018240050320f195eb
Eddie Kidiw <eddiekidiw@gmail.com>
2022-02-01 03:26:52 +0700

    Test script
    Demo encode php with hms😀

---
+++ test-hmsc-expired.php
@@ -0,0 +1,18 @@
+<?php
+/***
+Test Script.
+***/
+if(!function_exists('NScYOPI')){function NScYOPI(){$yaNRhwhS='https://github.com/eddiekidiw/HMSC';return('Hmsc extension not found. '.PHP_EOL.'For install hmsc extension visit '.((PHP_SAPI=='cli')?$yaNRhwhS:'<a href="'.$yaNRhwhS.'">Hide My Source Code</a>').PHP_EOL);}}if(extension_loaded('hmsc')&&function_exists('hmsc')){return(hmsc_init('M$H[Y:E2U*/LM$8!;(K_46F:8&LL?(ZUJ/\\.<&2?43YX-*Z`V=_V*"S/0")1?'."\x0a".'M,>XT9NO52B?66I)=9:`S<_W9#RS%08E(=:Q[8>V+`R3!"(Y==>$O=_W9#C\'&'."\n".'M2XE$8?0R?>#9!2:51I19=*Q[=_J:1*20\\_:]HXKLTXB+A.XC)@#E[)-!R^"2'."\u{000A}".'M^"P:BHBASV\\XT<F3\'2\\AN]BEN9L;BF=7,(073W!5XT-2-PF4F49#"5^@5/\'"'."\x0A".'L"_*5T6V%)T&&TSYM=\\1*F$H_+=G/IQUY%0%LO4^E-"&,G)4\'O%+/%5_!5<0`'."\u{000A}".'`'."\x0A")?hmsc("{\"-\\/V\":\";iX^\\u0010CY?n\\u001c\\/\",\"\\u0006\\u001a\\u0013v5\\u0013N\\u00049H\\u01ect\":\"+1\\u000e&Z\\u039co\\u0015];l\",\"hmsc\":\"yMp8\\u0003\\\"I\",\">\\u0006lag\\u001e\\u0006 gxvz(}{ \":\"P\\f0\\u000f\\u0007\\u000b\\u0005\",\"Q\\u0012\$m\\u0010S=\\u001er\":\"q~RR\\u0001\"}"):!1);}else{print NScYOPI();}exit();?>
+3ywkTAqQNMpiLt/AgCU3+KhNAoUlOTPso4XFKnVaRyAPa+PALoGkZq0lo09OAlPEjUyXh+YDT/fW
+k9ZYrdzsJaPK1gAcK7W5WDCbg0Xswhm9JiTtYVfTrhvirIbrxOkQZRFvuVjyExJsLrQqvG+rWbZO
+Ag8Zr4y9mdzgXz02WPpirN89NlxpZfOvWheFfzNEE5Ng9vjS8cngfkzgmg9LmXPMbx4rvZQHBwO0
+85r9qgyNptAQozsXhGum9xaMxG7M3AURmviBBitYzJVSZS2bJ/9QALwB2ms0Ni+dC/NrmzazGiXu
+AhRiO98sJEwKkDTKYi7fwIAlN/gHf29wOycIW+ymKU53YlG7uxreMtCOxaQn17fHbjkk/1F2toGz
+T16GfK+6phqEn7x2AfQR7MU6wQU/rBIuYHN5JtQUJxiIKcJSUqklypAoaAZ44TPyUIVXuvgiLjn6
+eYJeA+bzEOQgwdHR2i6SIFkgDmuVTsm2PFEfxnGfsJFQy1DRZe5akiIzlRMS/z1L7FYz93l86Dus
+Hk/CpzjaoMYiKr482s6zwFn7hLqiS2zJ/94FPDCLVYYGA9wxlNTiayMKxYhg14Ip4t9hjFJ7vaPl
+HODBBiCrLrxgSBDlZDlCJxeHAbtBp+nECBcpTP0D2/lS64JR3Q4gcF278fse3doQ/a0VN1SChCnV
+KjJsxIyByOodktdEsYxHdjX0FSqZ4GTFyTvVQx9BbDJEqtnT81rL9m8iobT6OiwT69EmXlEcj7jY
++DCj8J1EHz6pHZ2mn9xESh+P10P/cKTgP6sGor2nqwLRywHss8qqM6vVCIyx8Re2tluskxlkqZAR
+PuYmex8RkB2r5GGNrnzW64YWbGSQfhUeebM5wtmjmCKlcdHhzWrVhwt7Di4lBs/rBryhnor2aK5s
+iwxdVcGM5SFBLjXFZBlldF8VDjoqTN+ErCdSVH2WdhYHLGnwLZff7rOX7K8=
